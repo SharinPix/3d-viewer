f9688c26ef1e9e6a1faf5c661f79a03576e6d21c
Zafir Sk Heerah <zafir.heerah@spoonconsulting.com>
2024-06-24 16:51:36 +0400

    Test this

--- replace.sh
+++ replace.sh
@@ -7,6 +7,3 @@ replacement=$1
 
 # replace /wasm/ with commitnumber/wasm/ in model-loader.ts
 sed -i.bak "s/\/wasm/$replacement\/wasm/g" src/model-loader.ts
-
-# # replace styles.css with commitnumber/style.css in index.html
-sed -i.bak "s/styles.css/$replacement\/styles.css/g" src/templates/index.html

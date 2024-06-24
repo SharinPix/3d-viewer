baca2e669ec30561fc8f3911c89719e68d3d5d05
Zafir Sk Heerah <zafir.heerah@spoonconsulting.com>
2024-06-24 16:58:42 +0400

    Refactor

--- replace.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$1" ]; then
-  echo "Usage: sh replace.sh <replacement>"
-  exit 1
-fi
-
-replacement=$1
-
-# replace /wasm/ with commitnumber/wasm/ in model-loader.ts
-sed -i.bak "s/\/wasm/$replacement\/wasm/g" src/model-loader.ts
